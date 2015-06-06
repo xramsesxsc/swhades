e34e604d1e88667e1f08507615d23a3700202009
xramsesxsc <softwarestrada@gmail.com>
2015-06-05 23:33:08 -0500

    Update Form1.cs

--- appFerreteria/appFerreteria/Form1.cs
+++ appFerreteria/appFerreteria/Form1.cs
@@ -21,7 +21,7 @@ namespace appFerreteria
             int a = Convert.ToInt16(textBox1.Text);
             int b = Convert.ToInt16(textBox2.Text);
             
-            label1.Text = Convert.ToInt16(a + b).ToString() + "-> suma total loca";
+            label1.Text = Convert.ToInt16(a + b).ToString() + "-> joder suma total loca";
             
         }
     }
